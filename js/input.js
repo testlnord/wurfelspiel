390404c077672f42cb7369cd0bced3e786a9c15c
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-02 21:51:20 +0300

    Try using bootstrap template

--- js/input.js
+++ js/input.js
@@ -22,6 +22,25 @@ class WordsForm extends React.Component {
          placeholder="Enter your words..." onChange={this.handleChange} />
         <input type="submit" value="Submit" />
       </form>
+      
+      // <form class="form-horizontal" action = "game.html" method="GET" onSubmit={this.handleSubmit}>
+      // <fieldset>
+      // <div class="form-group">
+      //   <label class="col-md-12 control-label" for="textarea"></label>
+      //   <div class="col-sm-9">                     
+      //     <textarea class="form-control" id="textarea" value={this.state.value}
+      //     name="var" onChange={this.handleChange}>Enter your words</textarea>
+      //   </div>
+      // </div>
+      // <div class="form-group">
+      //   <label class="col-md-12 control-label" for="Submit"></label>
+      //   <div class="col-md-12">
+      //     <button id="Submit" name="Submit" class="btn btn-primary">Submit</button>
+      //   </div>
+      // </div>
+
+      // </fieldset>
+      // </form>
     );
   }
 }
