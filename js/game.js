114617f5bf7781875b54b0f8fe37797b9842a582
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-02 13:58:48 +0300

    Add parseInput function

--- js/game.js
+++ js/game.js
@@ -2,42 +2,29 @@ class Dice extends React.Component {
   constructor() {
     super();
     this.state = {
-    		value: '',
-			words: location.search.substring(5).split('+')
+    	value: '',
+			input: decodeURIComponent(location.search.substring(5))
 		};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  parseInput(input) {
+    return input.split('\n\r').map(x => x.split('\n').filter(Boolean));
+  }
+
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
-	var i=0, doc = document, docFrag = document.createDocumentFragment();
-	for (; i < Number(this.state.value) ; i++){
-	     var elem = doc.createElement('input');
-	     elem.type = 'button';
-	     elem.value = this.state.words[i];
-	     docFrag.appendChild(elem);
-	}
-	doc.body.appendChild(docFrag);
 
-    event.preventDefault();
-}
+  handleSubmit(event) {
+  }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick the number of dices:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-          </select>
-        </label>
         <input type="submit" value="Submit" />
       </form>
     );
