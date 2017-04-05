0c348752efef0401e6ff0202e6aa05c9b9af2e1c
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-05 16:42:23 +0300

    Add function that checks if an image is a URL

--- js/game.js
+++ js/game.js
@@ -32,6 +32,9 @@ class Dice extends React.Component {
   }
 
   image(word) {
+    var expr = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expr);
+    return word.match(regex);
 
   }
 
