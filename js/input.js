4c7c5fbc791e3c8ff2524e23724029aa5280c478
nfadeeva <fadeevanastasiia@gmail.com>
2017-04-05 15:18:03 +0300

    Fix input-page's styles

--- js/input.js
+++ js/input.js
@@ -17,30 +17,30 @@ class WordsForm extends React.Component {
 
   render() {
     return (
-      <form action = "game.html" method="GET" onSubmit={this.handleSubmit}>
-        <textarea value={this.state.value} name="var" rows="15" cols="40"
-         placeholder="Enter your words..." onChange={this.handleChange} />
-        <input type="submit" value="Submit" />
-      </form>
-      
-      // <form class="form-horizontal" action = "game.html" method="GET" onSubmit={this.handleSubmit}>
-      // <fieldset>
-      // <div class="form-group">
-      //   <label class="col-md-12 control-label" for="textarea"></label>
-      //   <div class="col-sm-9">                     
-      //     <textarea class="form-control" id="textarea" value={this.state.value}
-      //     name="var" onChange={this.handleChange}>Enter your words</textarea>
-      //   </div>
-      // </div>
-      // <div class="form-group">
-      //   <label class="col-md-12 control-label" for="Submit"></label>
-      //   <div class="col-md-12">
-      //     <button id="Submit" name="Submit" class="btn btn-primary">Submit</button>
-      //   </div>
-      // </div>
-
-      // </fieldset>
+      // <form action = "game.html" method="GET" onSubmit={this.handleSubmit}>
+      //   <textarea value={this.state.value} name="var" rows="15" cols="40"
+      //    placeholder="Enter your words..." onChange={this.handleChange} />
+      //   <input type="submit" value="Submit" />
       // </form>
+      
+      <form className="form-horizontal" action = "game.html" method="GET" onSubmit={this.handleSubmit}>
+      <fieldset>
+      <div className="form-group">
+        <label className="col-md-12 control-label" for="textarea"></label>
+        <div className="col-sm-9">                     
+          <textarea className="form-control" id="textarea" value={this.state.value}
+          name="var" onChange={this.handleChange}>Enter your words</textarea>
+        </div>
+      </div>
+      <div className="form-group">
+        <label className="col-md-12 control-label" for="Submit"></label>
+        <div className="col-md-12">
+          <button id="Submit" name="Submit" className="btn btn-primary">Submit</button>
+        </div>
+      </div>
+
+      </fieldset>
+      </form>
     );
   }
 }
